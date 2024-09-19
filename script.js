348df04c9eeed189ca071ae63d2bc00cfe870c3d
Nithishkumars-2001 <nithishkumar70102@gmail.com>
2024-09-19 17:30:36 +0530

    update all file

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 //select profile icon
-
+var home_page = document.querySelector(".home-page")
 var profile_icon =  document.querySelector(".profile-icon")
 var profile_icon_option = document.querySelector(".profile-icon-option")
 var display = 0
@@ -35,6 +35,8 @@ var my_profile_box = document.querySelector(".my-profile-box")
 my_profile_show.addEventListener("click", () => {
     my_profile_box.style.display = "block"
     profile_icon_option.style.display = "none"
+    home_page.style.display = "none"
+    courses_box.style.display = "none"
 })
 
 //select my profile edit box
@@ -141,3 +143,96 @@ change_password_box_show.addEventListener("click", () => {
 close_change_password.addEventListener("click", () => {
     change_password_box.style.display = "none"
 })
+
+//select home page
+var home_btn = document.querySelector(".home-btn")
+
+function show_home(){
+    courses_box.style.display = "none"
+    my_profile_box.style.display = "none"
+    home_page.style.display = "block"
+}
+
+
+//Learn Tech from Latest Courses slider
+var counter = 1;
+setInterval(function(){
+    document.getElementById('radio' + counter).checked = true;
+    counter++;
+    if(counter > 8){
+        counter = 1;
+    }
+}, 3000);
+
+
+// select courses option
+var course_option1 = document.querySelector(".course-option1")
+var course_option2 = document.querySelector(".course-option2")
+var course_option3 = document.querySelector(".course-option3")
+var course_option4 = document.querySelector(".course-option4")
+
+var courses_item = document.querySelector(".courses-item")
+var combo = document.querySelector(".combo-items")
+var free_library = document.querySelector(".free-library-items")
+var webinars_items = document.querySelector(".webinars-items")
+
+var line_1 = document.querySelector(".line1")
+var line_2 = document.querySelector(".line2")
+var line_3 = document.querySelector(".line3")
+var line_4 = document.querySelector(".line4")
+
+course_option1.addEventListener("click", () => {
+    courses_item.style.display = "flex"
+    combo.style.display = "none"
+    free_library.style.display = "none"
+    webinars_items.style.display = "none"
+    line_1.style.display = "block"
+    line_2.style.display = "none"
+    line_3.style.display = "none"
+    line_4.style.display = "none"
+})
+
+course_option2.addEventListener("click", () => {
+    combo.style.display = "flex"
+    courses_item.style.display = "none"
+    free_library.style.display = "none" 
+    webinars_items.style.display = "none"
+    line_1.style.display = "none"
+    line_2.style.display = "block"
+    line_3.style.display = "none"
+    line_4.style.display = "none"
+})
+
+course_option3.addEventListener("click", (event) => {
+    event.preventDefault();
+    combo.style.display = "none"
+    courses_item.style.display = "none"
+    free_library.style.display = "flex"
+    webinars_items.style.display = "none"
+    line_1.style.display = "none"
+    line_2.style.display = "none"
+    line_3.style.display = "block"
+    line_4.style.display = "none"
+})
+
+course_option4.addEventListener("click", (event) => {
+    event.preventDefault();
+    combo.style.display = "none"
+    courses_item.style.display = "none"
+    free_library.style.display = "none"
+    webinars_items.style.display = "flex"
+    line_1.style.display = "none"
+    line_2.style.display = "none"
+    line_3.style.display = "none"
+    line_4.style.display = "block"
+})
+
+// select courses box
+var courses_box = document.querySelector(".courses-box")
+
+function show_courses_box(){
+    courses_box.style.display = "block"
+    home_page.style.display = "none"
+    my_profile_box.style.display = "none"
+}
+
